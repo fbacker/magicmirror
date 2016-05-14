5e63fbd7ca917a5d307495702fe0ae4d1aa57fa4
Fredrick Bäcker <fredrick.backer@gmail.com>
2016-05-14 16:30:28 +0200

    remove bower components

--- bower_components/bragi-browser/lib/bragi/transports/index.js
+++
@@ -1,8 +0,0 @@
-/* =========================================================================
- *
- * index.js
- *      Exports all available transports
- *
- * ========================================================================= */
-module.exports.Console = require('./Console');
-module.exports.History = require('./History');
